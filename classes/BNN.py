08fc146ffbda6ac117832091bc0abcb2273a2689
coleman-zachery <42438576+coleman-zachery@users.noreply.github.com>
2023-10-20 13:59:26 -0400

    added unit tests (true/false tables)

--- classes/BNN.py
+++ classes/BNN.py
@@ -1,7 +1,7 @@
 from random import randint
 
 RULES = {}
-RULES['population'] = 1 << 10       # 1_024
+RULES['population'] = 1 << 7        # 2 ** (X + 1)
 RULES['network']    = [2, 0, 1]     # number of nodes in layers A, B, and C
 RULES['mutation']   = 0.5           # mutation rate
 
@@ -26,14 +26,16 @@ class Bunny:
         self.O = 0                      # output binary values
         self.G = 0                      # bunny genome
         self.S = 0                      # store bunny size
+        self.Size()
         self.N = 1 << self.A - 1        # input and hidden binary values
         if rand:                        # randomizes bunny genome
-            self.G = randint(1, 1 << self.Size())
+            self.G = randint(1, (1 << self.S) - 1)
     def Size(self):                     # computes length of bunny genome
         if self.S == 0:
             self.S = self.A * self.C + self.B * (self.A + self.C) + self.B * (self.B - 1) // 2
         return self.S
     def Propogate(self):                # computes self.O from self.G, self.S, and self.N
+        self.O = 0
         P = self.G + 0
         for i in range(self.B - 1):
             P = Cycle(P, self.S, self.C * (i + 1), self.A + self.C + self.B + self.B * 2 * i - i * (i - 1) // 2 - 1, self.C)
@@ -66,8 +68,11 @@ def Activate(n):
 def Bits(n):
     return (bin(n)).count('1')
 
-def Int2Bin(n):
-    return bin(n)[2:]
+def Int2Bin(n, s=-1):
+    b = bin(n)[2:]
+    if s != -1:
+        return ((s - len(b)) * '0') + b
+    return b
 
 def Bin2Int(n):
     return int(n, 2)
@@ -81,13 +86,56 @@ def Bin2Int(n):
 
     def ShortCoat(self):
         return None
-    
+
     def LongCoat(self):
         return None
-    
+
     def BrushCoat(self):
         return None
 
     def Shed(self, shed=True):
         return None
 """
+
+def Int2Str(n, l):
+    return ' ' * max(0, l - len(str(n))) + str(n)
+
+A = 3
+B = 0
+C = 1
+
+"""
+    INSTRUMENT         T/F REPRESENTATION    min len of B for 100%
+    False              0, 0, 0, 0            0
+    A and B            0, 0, 0, 1            1
+    not A and B        0, 0, 1, 0            ...
+    B                  0, 0, 1, 1            0
+    A and not B        0, 1, 0, 0            ...
+    A                  0, 1, 0, 1            0
+    A xor B            0, 1, 1, 0            0
+    A or B             0, 1, 1, 1            ...
+    not A and not B    1, 0, 0, 0            ...
+    not (A xor B)      1, 0, 0, 1            0
+    not A and B        1, 0, 1, 0            0
+    not A or B         1, 0, 1, 1            ...
+    not B              1, 1, 0, 0            0
+    A or not B         1, 1, 0, 1            ...
+    not (A and B)      1, 1, 1, 0            ...
+    True               1, 1, 1, 1            0
+"""
+
+base = Bunny(A, B, C, False)
+S = base.Size()
+
+for i in range(1 << S):
+    b = Bunny(A, B, C, False)
+    b.G = i
+
+    for j in range(4):
+        b.N = 4 + j
+        b.Propogate()
+        #       0,0   0,1   1,0   1,1
+        test = [1,    1,    1,    1][j]
+        b.F += b.O == test
+
+    print(b.F, Int2Str(i, len(str(1 << S))), Int2Bin(b.G, S))
