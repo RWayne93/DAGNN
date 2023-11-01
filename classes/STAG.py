589cc315cacb391182cd5664050b0a59e36ae50b
coleman-zachery <42438576+coleman-zachery@users.noreply.github.com>
2023-11-01 13:52:46 -0400

    implemented straightforward .Learn(unit_tests, max_gens) method

--- classes/STAG.py
+++ classes/STAG.py
@@ -1,15 +1,12 @@
 from random import random as R
 
 # Sorted Topological Acyclical Graph
-
 class STAG:
     def __init__(self, input_size, hidden_size, output_size):
-        self.A = input_size
-        self.B = hidden_size
-        self.C = output_size
-        self.D = [] # node ids
-        self.E = 1 # error
-        self.F = 0 # fitness
+        self.A = tuple(['A' + str(i).zfill(len(str(input_size - 1))) for i in range(input_size)]) # input node ids
+        self.B = tuple(['B' + str(i).zfill(len(str(hidden_size - 1))) for i in range(hidden_size)]) # hidden node ids
+        self.C = tuple(['C' + str(i).zfill(len(str(output_size - 1))) for i in range(output_size)]) # output node ids
+        self.E = 1 # error / fitness
         self.I = [] # unit tests / input values
         self.L = [] # list of links
         self.N = {} # node data
@@ -19,71 +16,120 @@ class STAG:
         self.R = 0.001 # learning rate
         self.S = 0 # size / length of self.L
         self.T = 0.01 # threshold of error
-        self.W = [-1, 1] # initial domain of random weights
+        self.W = (-1, 1) # initial domain of random weights
 
-        # generate node ids
-        for i in range(self.A):
-            self.D += ['A' + str(i).zfill(len(str(self.A - 1)))]
-        for i in range(self.B):
-            self.D += ['B' + str(i).zfill(len(str(self.B - 1)))]
-        for i in range(self.C):
-            self.D += ['C' + str(i).zfill(len(str(self.C - 1)))]
-        self.D = tuple(self.D)
+        D = self.B + self.C
+        for node_1 in self.A + self.B:
+            # assign input and hidden node ids to node data
+            self.N |= {node_1: {'value': 0, 'links': {}}}
 
-        # generate values and links for each node id
-        for i in range(len(self.D)):
-            node_id = self.D[i]
-            self.N |= {node_id:{'value':0}}
-            if node_id[0] != 'C':
-                self.N[node_id]['links'] = {}
-                j = max(self.A, i + 1)
-                while j < len(self.D):
-                    node_out = self.D[j]
-                    self.L += [(node_id, node_out)]
-                    self.N[node_id]['links'][node_out] = self.RandomWeight()
-                    j += 1
+            # create link with random weight between input and hidden nodes to other hidden nodes and output nodes
+            j = 0
+            while j < len(D):
+                if D[j] == node_1: j += 1
+                node_2 = D[j]
+                self.L += [(node_1, node_2)]
+                self.N[node_1]['links'][node_2] = self.RandomWeight()
+                j += 1
+
+        # assign output node ids to node data
+        for node in self.C: self.N |= {node: {'value': 0}}
         self.L = tuple(self.L)
         self.S = len(self.L)
 
+    # return random weight between values of self.W
     def RandomWeight(self):
         return R() * (self.W[1] - self.W[0]) + self.W[0]
 
+    # propogate input node values through the network to compute output node values
     def Forward(self):
-        for n in self.N:
-            if n in self.I:
-                self.N[n]['value'] = self.I[n]
-            else:
-                self.N[n]['value'] = 0
-        i = 'A0'
-        for (a, b) in self.L:
-            if a[0] != 'A' and a != i:
-                self.N[a]['value'] = Activate(self.N[a]['value'])
-                i = a
-            self.N[b]['value'] += self.N[a]['value'] * self.N[a]['links'][b]
-        for n in self.N:
-            if n[0] == 'C':
-                self.N[n]['value'] = Activate(self.N[n]['value'])
-        self.O = dict([(node, self.N[node]['value']) for node in self.N if node[0] == 'C'])
-        self.E += sum([Error(self.O[i], self.P[i]) for i in self.P])
+        # set input nodes to values assigned by unit test
+        for node in self.A: self.N[node]['value'] = self.I[node]
+        # set hidden and output nodes to zero
+        for node in self.B + self.C: self.N[node]['value'] = 0
 
+        last_activated = None
+        for (node_1, node_2) in self.L:
+            # activate each hidden node once
+            if node_1[0] != 'A' and node_1 != last_activated:
+                self.N[node_1]['value'] = Activate(self.N[node_1]['value'])
+                last_activated = node_1
+            # sum product of node values and weights
+            self.N[node_2]['value'] += self.N[node_1]['value'] * self.N[node_1]['links'][node_2]
+
+        # activate output nodes
+        for node in self.C: self.N[node]['value'] = Activate(self.N[node]['value'])
+
+        # pass output nodes to self.O
+        for node in self.C: self.O |= {node: self.N[node]['value']}
+
+        # compute error / fitness as sum of differences squared between output nodes and expected outputs from unit test
+        for node in self.P: self.E += Error(self.O[node], self.P[node])
+
+    # select random links for later adjustment with learning rate
     def PickQ(self):
         self.Q = {}
         while len(self.Q) < (self.S ** .4) // 1:
             self.Q |= {self.L[int(len(self.L) * R())]: 0}
 
+    # compute error from passing test through network
+    def Test(self, tests):
+        self.E = 0
+        for inputs, outputs in tests:
+            self.I = inputs
+            self.P = outputs
+            self.Forward()
+
+    # machine learning for network
+    def Learn(self, tests, max_gens=1000):
+        gen = 0
+        # stop if max generations exceeded or error passes threshold
+        while gen < max_gens and self.E > self.T:
+            # get current error
+            self.Test(tests)
+            current_error = self.E + 0
+            print(gen, current_error)
+
+            # adjust learning rate for each selected Q-link
+            self.PickQ()
+            for Q in self.Q:
+                node_1, node_2 = Q
+
+                # temporarily adjust Q-link by learning rate
+                self.N[node_1]['links'][node_2] += self.R
+
+                # compute error difference after applying the learning rate
+                self.Test(tests)
+                self.Q[Q] = (current_error - self.E) / self.R
+
+                # remove learning rate from Q-link
+                self.N[node_1]['links'][node_2] -= self.R
+
+            # Sort Q links by magnitude of error difference
+            self.Q = [(key, val) for (key, val) in self.Q.items()]
+            (self.Q).sort(key = lambda x: abs(x[1]), reverse=True)
+
+            # update link in network with largest error gradient by learning rate
+            (node_1, node_2), err = self.Q[0]
+            self.N[node_1]['links'][node_2] += self.R * [1, -1][err < 0]
+
+            gen += 1
+        print(self.N)
+
+# node activation function: f(x) = x - tanh(x)
 def Activate(x):
     e = 2.7182818284590452
     a, b = e ** x, e ** (-x)
     return x - (a - b) / (a + b)
 
+# error function: difference squared
 def Error(x, y):
     return (x - y) ** 2
+"""
     a = (x - y) ** 2
     b = (2 * x - 1) * (2 * y - 1) < 0
     return 1 - (a + b)
-
-
-
+"""
 
 #######################################################################
 #######################################################################
@@ -92,8 +138,7 @@ def Error(x, y):
 
 # UNIT TESTING
 
-
-A, B, C = 7, 1, 1
+A, B, C = 3, 1, 1
 NN = STAG(A, B, C)
 
 unit_tests = [
@@ -103,38 +148,9 @@ unit_tests = [
     ({'A0':1, 'A1':1, 'A2':1}, {'C0':1})
 ]
 
-gen = 0
-max_gens = 9999
+# adjust learning rate, weights, and threshold
+NN.R = 0.005
+NN.W = [-1, 1]
+NN.T = 0.001
 
-while NN.E > NN.T and gen < max_gens:
-    NN.E = 0
-    for inputs, outputs in unit_tests:
-        NN.I = inputs
-        NN.P = outputs
-        NN.Forward()
-    current_error = NN.E + 0
-    print(gen, current_error)
-    NN.PickQ()
-    for Q in NN.Q:
-        a, b = Q
-        NN.N[a]['links'][b] += NN.R
-        NN.E = 0
-        for inputs, outputs in unit_tests:
-            NN.I = inputs
-            NN.P = outputs
-            NN.Forward()
-        NN.Q[Q] = (current_error - NN.E) / NN.R
-        NN.N[a]['links'][b] -= NN.R
-    Q = [(key, val) for (key, val) in NN.Q.items()]
-    Q.sort(key = lambda x: abs(x[1]), reverse=True)
-    (a, b), c = Q[0]
-    NN.N[a]['links'][b] += NN.R * [1, -1][c < 0]
-    gen += 1
-print(NN.N)
-
-for inputs, outputs in unit_tests:
-    NN.I = inputs
-    NN.Forward()
-    print(NN.I, NN.O)
-NN.Forward()
-print(NN.N)
+NN.Learn(unit_tests, max_gens=49999)
