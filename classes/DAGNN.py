dd4eabd0e233043f7ec106df13836c76c580f345
coleman-zachery <42438576+coleman-zachery@users.noreply.github.com>
2023-11-09 16:06:22 -0500

    Update DAGNN.py

--- classes/DAGNN.py
+++ classes/DAGNN.py
@@ -17,20 +17,26 @@ class DAGNN:
         self.nodes = [0] * (self.A + self.B + self.C)
         self.expected_output = [0] * self.C
 
-        # link matrices
-        self.links = np.triu(np.ones((self.A + self.B, self.B + self.C)), k=1-self.A)
-
         # hyper parameters
         self.error = 0
         self.fitness = 0
         self.max_generations = 2_000
         self.unit_tests = []
-        self.learning_rate = 0.001
+        self.learning_rate = 0.01
         self.threshold = 2
+        self.weights = (1, -1, -1, 1) # initial weights for links in each quadrant
 
-    def Randomize(self):
-        #randomized_array = np.random.uniform(lower_bound, upper_bound, size=array_size)
-        self.links = np.random.rand(*(self.links).shape)
+        # create and populate the links matrix
+        self.links = np.zeros((self.A + self.B, self.B + self.C))
+        
+        # assign weights for each quadrant
+        self.links[:self.A, :self.B] = self.weights[0]
+        self.links[:self.A, self.B:] = self.weights[1]
+        self.links[self.A:, :self.B] = np.triu(np.full((self.B, self.B), self.weights[2]), k=1)
+        self.links[self.A:, self.B:] = self.weights[3]
+
+
+
 
     # matrix multiplication and node activation
     def Forward(self):
@@ -52,7 +58,6 @@ class DAGNN:
         # C vector activation function
         vector_activate = np.vectorize(Activate)
         self.nodes[-self.C:] = vector_activate(self.nodes[-self.C:])
-        print(self.nodes)
 
         # mean squared error function for two vectors
         def MSE(vector_a, vector_b):
@@ -70,12 +75,18 @@ class DAGNN:
         # compound fitness
         self.fitness += Score(self.nodes[-self.C:], self.expected_output)
 
+
+
+
     # compute error and fitness from unit tests
     def Test(self):
         self.error, self.fitness = 0, 0
         for inputs, outputs in self.unit_tests:
             self.nodes[:self.A], self.expected_output = inputs, outputs
             self.Forward()
+
+
+
 
     # machine learning algorithm
     def Learn(self, minimize_error=False):
@@ -94,13 +105,60 @@ class DAGNN:
         current_generation = 0
         while current_generation < self.max_generations and learn_again:
 
-            # get current error
+            # store changes in error
             current_error = self.error + 0
+            Q = []
+            for i in range(2):
+                j = np.random.randint(0, self.A + self.B)
+                k = np.random.randint(max(0, j-self.A+1), self.B + self.C)
+                self.links[j][k] += self.learning_rate
+                self.Test()
+                Q += [[(j, k), (current_error - self.error) / self.learning_rate]]
+                self.links[j][k] -= self.learning_rate
+
+            # sort q links
+            Q.sort(key = lambda x: abs(x[1]), reverse=True)
+
+            # update link in network with largest error gradient by learning rate
+            (i, j), error = Q[0]
+            self.links[i][j] += self.learning_rate * [1, -1][error < 0]
+
+            # print every 100th generation
+            if current_generation % 100 == 0: print(f'gen: {current_generation}    error: {self.error:0.4f}    score: {self.fitness} / {len(self.unit_tests)}')
+            current_generation += 1
+
+            # test for next generation
+            self.Test()
+
+            # adjust learning rate based on adjustment success
+            if self.error < current_error:
+                self.learning_rate *= 1.1
+            else:
+                if self.learning_rate > initial_learning_rate:
+                    self.learning_rate = initial_learning_rate + 0
+                else:
+                    self.learning_rate /= 1.1
+
+            learn_again = self.fitness < len(self.unit_tests)
+
+            """if minimize_error:
+                learn_again = self.fitness == len(self.unit_tests)
+                if learn_again:
+                    learn_again &= self.error > self.threshold"""
+
+        self.learning_rate = initial_learning_rate + 0
+
         # incomplete...
 
+        print(f'gen: {current_generation}    error: {self.error:0.4f}    score: {self.fitness} / {len(self.unit_tests)}')
+        print(self.links)
+
+# unit testing
+
 NN = DAGNN({'A':7, 'B':3, 'C': 1})
-print(NN.links)
 unit_tests = [(list(map(int, bin(n)[2:])), [list(map(int, bin(n)[2:]))[1:5][n % 4]]) for n in range(64, 128)]
 
+print(NN.links)
+
 NN.unit_tests = unit_tests
-NN.Test()
+NN.Learn()
