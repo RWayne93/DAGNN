575a7236145ae2bf7bf7ca8c93bea1f769d34287
coleman-zachery <42438576+coleman-zachery@users.noreply.github.com>
2023-11-09 21:52:24 -0500

    Update DAGNN.py

--- classes/DAGNN.py
+++ classes/DAGNN.py
@@ -6,31 +6,30 @@ from math import tanh, log
 from random import randint
 
 
+
+
 # Directed Acyclical Graph Neural Network
 class DAGNN:
     def __init__(self, structure={'A':0, 'B':0, 'C':0}):
 
-        # store network structure size
-        self.A = structure['A']
-        self.B = structure['B']
-        self.C = structure['C']
+        # store network structure and size
+        self.A, self.B, self.C = structure['A'], structure['B'], structure['C']
+        self.size = self.A * (self.B + self.C) + self.B * (self.B - 1) // 2 + self.B * self.C
 
         # node vectors
         self.nodes = [0] * (self.A + self.B + self.C)
         self.expected_output = [0] * self.C
 
         # hyper parameters
-        self.error = 0
-        self.fitness = 0
+        self.error, self.fitness = 0, 0
         self.max_generations = 10_000
         self.unit_tests = []
         self.learning_rate = 0.01
         self.threshold = 2
+        self.pruning = 0
 
-        # initial weights for links in each quadrant
-        self.weights = (0, 0, 0, 1)
-
-        # create and populate the links matrix, and assign weights for each quadrant
+        # create matrix and assign initial weights for each quadrant
+        self.weights = (-.1, .1, -.1, 1)
         self.links = [
             [self.weights[0] if i < self.A and j < self.B else
             self.weights[1] if i < self.A else
@@ -40,7 +39,14 @@ class DAGNN:
             for j in range(self.B + self.C)]
             for i in range(self.A + self.B)
         ]
-        print(self.links)
+        self.previous_links = str(self.links)
+
+
+
+
+    # count number of non-zero weights in links
+    def Size(self):
+        return sum(1 for i in self.links for j in i if j != 0)
 
     # B node activation function
     def Activate(self, x):
@@ -58,26 +64,30 @@ class DAGNN:
     def DotProduct(self, a, b):
         return [sum(sublist[i] * j for j, sublist in zip(a, b)) for i in range(len(b[0]))]
 
+
+
+
     # matrix multiplication and node activation
     def Forward(self):
 
-        # set initial B and C nodes from A links to B and C
+        # set initial B and C nodes from A links
         self.nodes[self.A:] = self.DotProduct(self.nodes[:self.A], self.links[:self.A])
 
-        # activate B nodes and then compute B and C nodes from B links to B and C
+        # activate each B node and then compute next B and C nodes from B links
         for i, _ in enumerate(self.nodes[self.A:-1]):
             j = i + self.A
             self.nodes[j] = self.Activate(self.nodes[j])
             self.nodes[j+1:] = [a + b * self.nodes[j] for a, b in zip(self.nodes[j+1:], self.links[j][i+1:])]
 
-        # C vector activation function
+        # activate all nodes in C
         self.nodes[-self.C:] = [self.Activate(x) for x in self.nodes[-self.C:]]
 
-        # compound error
+        # compound error and fitness
         self.error += self.MSE(self.nodes[-self.C:], self.expected_output)
-
-        # compound fitness
         self.fitness += self.Score(self.nodes[-self.C:], self.expected_output)
+
+
+
 
     # compute error and fitness from unit tests
     def Test(self):
@@ -86,79 +96,100 @@ class DAGNN:
             self.nodes[:self.A], self.expected_output = inputs, outputs
             self.Forward()
 
-    # machine learning algorithm
-    def Learn(self, minimize_error=False):
 
-        # get initial fitness of network
-        self.Test()
-        learn_again = self.fitness < len(self.unit_tests)
-        if minimize_error:
-            print('\n :: minimizing error ::\n')
-            learn_again = self.fitness == len(self.unit_tests) and self.error > self.threshold
-        else:
-            print('\n :: learning ::\n')
 
-        # stop learning if reached max generations or fitness is 100%
-        initial_learning_rate = self.learning_rate + 0
-        current_generation = 0
-        while current_generation < self.max_generations and learn_again:
 
-            # store changes in error
-            current_error = self.error + 0
-            Q = []
-            for i in range(1):
-                j = randint(0, self.A + self.B - 1)
-                k = randint(max(0, j-self.A+1), self.B + self.C - 1)
-                self.links[j][k] += self.learning_rate
-                self.Test()
-                Q += [[(j, k), (current_error - self.error) / self.learning_rate]]
-                self.links[j][k] -= self.learning_rate
-
-            # sort q links
-            #Q.sort(key = lambda x: abs(x[1]), reverse=True)
-
-            # update link in network with largest error gradient by learning rate
-            (i, j), error = Q[0]
-            self.links[i][j] += self.learning_rate * [1, -1][error < 0]
-
-            # print every 100th generation
+    def Learn(self):
+        print('\n :: minimizing error ::\n') if self.pruning == 3 else print('\n :: learning ::\n')
+        initial_learning_rate, current_generation = self.learning_rate + 0, 0
+        learn_again = True
+        while learn_again:
+            learn_again = self.fitness < len(self.unit_tests) and current_generation <= self.max_generations
+            if self.pruning == 3: learn_again = self.fitness == len(self.unit_tests) and current_error <= self.max_generations and self.error > self.threshold
+            # get current error and fitness
+            self.Test()
             if current_generation % 100 == 0:
                 print(f'gen: {current_generation}    error: {self.error:0.4f}    score: {self.fitness} / {len(self.unit_tests)}')
-
-            current_generation += 1
-
-            # test for next generation
-            self.Test()
-
-            # adjust learning rate based on adjustment success
-            if self.error < current_error:
-                self.learning_rate *= 1.1
-            else:
-                if self.learning_rate > initial_learning_rate:
-                    self.learning_rate = initial_learning_rate + 0
+            # adjust random weight and learning rate
+            current_error = self.error + 0
+            i = randint(0, self.A + self.B - 1)
+            j = randint(max(0, i - self.A + 1), self.B + self.C - 1)
+            if self.links[i][j] != 0:
+                self.links[i][j] -= self.learning_rate
+                self.Test()
+                if self.error < current_error:
+                    self.learning_rate *= 1.1
                 else:
-                    self.learning_rate /= 1.1
-
-            learn_again = self.fitness < len(self.unit_tests)
-
-            """if minimize_error:
-                learn_again = self.fitness == len(self.unit_tests)
-                if learn_again:
-                    learn_again &= self.error > self.threshold"""
-
+                    self.learning_rate = max(self.learning_rate / 1.1, initial_learning_rate)
+                    if self.error > current_error:
+                        self.links[i][j] += self.learning_rate * 2
+                    else:
+                        self.links[i][j] += self.learning_rate
+            current_generation += 1
+        # stopped learning
         self.learning_rate = initial_learning_rate + 0
+        if current_generation > self.max_generations:
+            print('\n :: max generations reached ::\n')
+            prompt = 'pruning' if self.pruning == 1 else 'minimizing' if self.pruning == 3 else 'learning'
+            again = input(f'continue {prompt} (<enter> for yes)?: ')
+            if again == '': self.Learn()
+            else:
+                if self.pruning == 1:
+                    print('\n :: finished pruning ::\n')
+                else:
+                    print('\n :: current failed end-state ::\n')
+                self.pruning = 2
+                for i in self.links: print(i)
+        else:
+            if current_generation % 100 != 0: print(f'gen: {current_generation}    error: {self.error:0.4f}    score: {self.fitness} / {len(self.unit_tests)}')
+            print(f'\n :: learning complete ::\n') if self.pruning != 3 else print(f'\n :: minimizing complete ::\n')
+            for i in self.links: print(i)
+            if self.pruning == 0: self.Prune()
 
-        print(f'gen: {current_generation}    error: {self.error:0.4f}    score: {self.fitness} / {len(self.unit_tests)}')
-        for i in self.links: print(i)
-        print(self.nodes)
+
+
 
     def Prune(self):
-
+        print('\n :: pruning ::')
+        self.pruning = 1
         self.max_generations //= 10
-        while self.prune_again:
+        while self.pruning == 1:
+            # save previous links
+            self.previous_links = str(self.links)
+            # find smallest non-zero weight
+            smallest_weight = None
+            for i, row in enumerate(self.links):
+                for j, weight in enumerate(row):
+                    if weight != 0:
+                        if smallest_weight == None or abs(weight) < smallest_weight[1]:
+                            smallest_weight = [[i, j], abs(weight)]
+            # delete smallest weight
+            i, j = smallest_weight[0]
+            self.links[i][j] = 0
+            print(f'\nremoving: ({i}, {j})')
+            # re-evaluate
+            self.Test()
+            self.Learn()
 
-            print('\n :: pruning ::')
+        # restore previous links and minimize error
+        self.links = eval(self.previous_links)
+        self.max_generations *= 10
+        self.pruning = 3
+        self.Learn()
 
+        # restore last successful links
+        self.links = eval(self.previous_links)
+        self.Test()
+        for i in self.links: print(i)
+        current_size = self.Size()
+        print(f'\n :: pruning complete. pruned {self.size - current_size} weights. current size = {current_size} ::')
+        print(f'\n :: final unit testing.    error: {self.error:0.4f}    score: {self.fitness} / {len(self.unit_tests)} ::\n')
+
+
+
+
+# unit testing
+# unit testing
 # unit testing
 
 NN = DAGNN({'A':7, 'B':3, 'C': 1})
